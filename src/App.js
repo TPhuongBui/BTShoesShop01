51024bfba4fea0f7650aa36e84b3992dba42e154
TPhuongBui <thanhphuong.bui2018@gmail.com>
2023-01-07 18:43:41 +0700

    tạo mới

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import BTShoesStore from './BTShoesShop01/BTShoesStore';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BTShoesStore/>
+    </>);
 }
 
 export default App;
